b6c19a3f377e0499dcc59cecefb9aa3f071b3926
ericcalasans <ericcalasans@gmail.com>
2020-03-02 21:09:33 -0300

    Projeto 1 - parei no video 63

--- projeto01/app.js
+++ projeto01/app.js
@@ -2,7 +2,7 @@ new Vue({
     el: '#app',
 
     data: {
-        playerLife: 0,
+        playerLife: 100,
         monsterLife: 100,
         running: false,
     },
@@ -12,12 +12,33 @@ new Vue({
             return this.playerLife == 0 || this.monsterLife == 0
         }
     },
+    methods:  {
+        startGame(){
+            this.running = true
+            this.playerLife = 100
+            this.monsterLife = 100
+        },
+        attack(especial){
+            this.hurt(7, 12, false)
+            console.log(this.hasResult)
+        },
 
-    watched:  {
-
+        hurt(min, max, especial){
+            const plus = especial ? 5 : 0
+            const hurt = this.getRandom(min + plus, max + plus)
+            this.playerLife = Math.max(this.playerLife - hurt, 0)
+        },
+        getRandom(min, max){
+            const value = Math.random() * (max - min) + min
+            return Math.round(value)
+        }
     },
 
-    methods:  {
-
-    }
+    watch:  {
+        hasResult(value) {
+            if (value) {
+                this.running = false
+            }
+        }
+    },
 })
